caeaa8cee74d9be8b4134cb56edb541bf20f8a86
zhongyizh <zhongyi.zhao@tufts.edu>
2024-06-27 15:59:27 -0400

    fix detail page and getPostData

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -72,126 +72,125 @@ Page({
         }
         });
 	},
-	onTapContact() {
-		// TODO: On Hold/已售出
-		const postData = this.data.postData
-		const phone = postData.phone ? '手机号: ' + postData.phone : '';
-		const email = postData.emailAddress ? '邮箱：' + postData.emailAddress : '';
-		let contact = '';
-		if (phone && email) {
-			contact = phone + '\n' + email;
-		} else if (phone) {
-			contact = phone;
-		} else if (email) {
-			contact = email;
-		}
-		console.log('成功复制到剪贴板：' + contact)
-		wx.setClipboardData({
-			data: contact,
-			success: function(){
-				wx.showToast({
-					title: '复制成功',  
-					icon: 'success',    
-					duration: 2000,
-				});
-			}
-		})
-  },
-
-  isOwner: async function() {
-      // 先获取当前帖子作者的openId
-      let authorOpenId = this.data.postData._openid;
-      return new Promise((resolve, reject) => {
-          wx.cloud.callFunction({
-              name: 'getUserInfo',
-              success: function(res) {
-                  // 再使用云函数获取用户的openId
-                  let userOpenId = res.result._id;
-                  // 比较是不是一个人，如果是的话就说明当前用户是帖子的所有者
-                  console.log("detail.js: isOwner(): trynna check if current user owns the post: " + userOpenId + 
-                              (userOpenId == authorOpenId ? " 是 " : " 不是 ") + authorOpenId);
-                  resolve(userOpenId == authorOpenId);
-              },
-              fail: function(res) {
-                  console.log("detail.js: isOwner(): get user info failed! ");
-                  reject(false);
-              }
-          })
-      });
-  },
-  editPost: function() {
-      console.log("detail.js: editPost(): ",this.data.postData);
-      wx.navigateTo({
-          url: ( this.data.postData.postType === "selling" ? 
-              '/pages/post/new-post-listing/new-post-listing' :
-              '/pages/post/new-post/new-post'),
-          success: (res)=>{
-              // 发送帖子编辑event和当前详情页数据至帖子编辑页
-              res.eventChannel.emit('onPageEdit',
-                  {
-                      _id: this.data.postData._id,
-                      fileList: this.data.postData.imageUrls.map((i) => {
-                          return {
-                              url: i,
-                              imageSize: 0,
-                              overSize: false
-                          }
-                      }),
-                      price: this.data.postData.price,
-                      condition: this.data.postData.condition || ListingConditions.UNKNOWN,
-                      // ddl: this.data.postData.ddl,
-                      body: this.data.postData.body,
-                      title: this.data.postData.title
-                  }
-              );
-          }
-      })
-  },
-  onDeletePostBTNClicked: function() { 
-      new Promise((resolve, reject) => {
-          wx.showModal({
-              title: "确认删除？",
-              content: "删除的帖子将不可恢复",
-              success: function(res) {
-                  resolve(res.confirm);
-              }
-          })
-      }).then(isConfirmed => {
-          if (!isConfirmed) return;
-          wx.showLoading({
-              title: '删除中...',
-              mask: true
-          })
-          deletePost(this.data.postData._id, this.data.postData.imageUrls).then(() => {
-              wx.hideLoading();
-              wx.navigateBack();
-          }); 
-      });
-  } 
-  // 分享给朋友
-  onShareAppMessage: function() {
+    onTapContact() {
+        // TODO: On Hold/已售出
+        const postData = this.data.postData
+        const phone = postData.phone ? '手机号: ' + postData.phone : '';
+        const email = postData.emailAddress ? '邮箱：' + postData.emailAddress : '';
+        let contact = '';
+        if (phone && email) {
+            contact = phone + '\n' + email;
+        } else if (phone) {
+            contact = phone;
+        } else if (email) {
+            contact = email;
+        }
+        console.log('成功复制到剪贴板：' + contact)
+        wx.setClipboardData({
+            data: contact,
+            success: function(){
+                wx.showToast({
+                    title: '复制成功',  
+                    icon: 'success',    
+                    duration: 2000,
+                });
+            }
+        })
+    },
+    isOwner: async function() {
+        // 先获取当前帖子作者的openId
+        let authorOpenId = this.data.postData._openid;
+        return new Promise((resolve, reject) => {
+            wx.cloud.callFunction({
+                name: 'getUserInfo',
+                success: function(res) {
+                    // 再使用云函数获取用户的openId
+                    let userOpenId = res.result._id;
+                    // 比较是不是一个人，如果是的话就说明当前用户是帖子的所有者
+                    console.log("detail.js: isOwner(): trynna check if current user owns the post: " + userOpenId + 
+                                (userOpenId == authorOpenId ? " 是 " : " 不是 ") + authorOpenId);
+                    resolve(userOpenId == authorOpenId);
+                },
+                fail: function(res) {
+                    console.log("detail.js: isOwner(): get user info failed! ");
+                    reject(false);
+                }
+            })
+        });
+    },
+    editPost: function() {
+        console.log("detail.js: editPost(): ",this.data.postData);
+        wx.navigateTo({
+            url: ( this.data.postData.postType === "selling" ? 
+                '/pages/post/new-post-listing/new-post-listing' :
+                '/pages/post/new-post/new-post'),
+            success: (res)=>{
+                // 发送帖子编辑event和当前详情页数据至帖子编辑页
+                res.eventChannel.emit('onPageEdit',
+                    {
+                        _id: this.data.postData._id,
+                        fileList: this.data.postData.imageUrls.map((i) => {
+                            return {
+                                url: i,
+                                imageSize: 0,
+                                overSize: false
+                            }
+                        }),
+                        price: this.data.postData.price,
+                        condition: this.data.postData.condition || ListingConditions.UNKNOWN,
+                        // ddl: this.data.postData.ddl,
+                        body: this.data.postData.body,
+                        title: this.data.postData.title
+                    }
+                );
+            }
+        })
+    },
+    onDeletePostBTNClicked: function() { 
+        new Promise((resolve, reject) => {
+            wx.showModal({
+                title: "确认删除？",
+                content: "删除的帖子将不可恢复",
+                success: function(res) {
+                    resolve(res.confirm);
+                }
+            })
+        }).then(isConfirmed => {
+            if (!isConfirmed) return;
+            wx.showLoading({
+                title: '删除中...',
+                mask: true
+            })
+            deletePost(this.data.postData._id, this.data.postData.imageUrls).then(() => {
+                wx.hideLoading();
+                wx.navigateBack();
+            }); 
+        });
+    },
+    // 分享给朋友
+    onShareAppMessage: function() {
     const detailData = JSON.stringify(this.data.postData)
-    return {
-      title: this.data.postData.nickname + '发布的帖子: ' + this.data.postData.title,
-      path: `/pages/detail/detail?data=${detailData}`,
-      imageUrl: this.data.postData.imageUrls[0],
-      success: function() {
-        // 分享成功后的回调
-        console.log('分享成功');
-      },
-      fail: function() {
-        // 分享失败后的回调
-        console.log('分享失败');
-      }
-    };
-  },
-  // 分享到朋友圈
-  onShareTimeline: function() {
+        return {
+            title: this.data.postData.nickname + '发布的帖子: ' + this.data.postData.title,
+            path: `/pages/detail/detail?data=${detailData}`,
+            imageUrl: this.data.postData.imageUrls[0],
+            success: function() {
+                // 分享成功后的回调
+                console.log('分享成功');
+            },
+            fail: function() {
+                // 分享失败后的回调
+                console.log('分享失败');
+            }
+        };
+    },
+    // 分享到朋友圈
+    onShareTimeline: function() {
     const detailData = JSON.stringify(this.data.postData)
-    return {
-      title: this.data.postData.nickname + '发布的帖子: ' + this.data.postData.title,
-      path: `/pages/detail/detail?data=${detailData}`,
-      imageUrl: this.data.postData.imageUrls[0] 
-    };
-  },
+        return {
+            title: this.data.postData.nickname + '发布的帖子: ' + this.data.postData.title,
+            path: `/pages/detail/detail?data=${detailData}`,
+            imageUrl: this.data.postData.imageUrls[0] 
+        };
+    },
 })
