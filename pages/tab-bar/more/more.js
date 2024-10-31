78b0770fd63e37f107db2c707a18fa0f6c73d03d
Yichen Liu <ycliuleo0601@gmail.com>
2024-10-31 14:45:47 +0800

    除客服之外完成

--- pages/tab-bar/more/more.js
+++ pages/tab-bar/more/more.js
@@ -2,12 +2,76 @@
 Page({
 	data: {
 		currentTabbarIndex: 4,
+		version: "V0.4.5"
 	},
 	onLoad() {
-		if(typeof this.getTabBar === 'function' && this.getTabBar()) {
+		if (typeof this.getTabBar === 'function' && this.getTabBar()) {
 			this.getTabBar().setData({
 				selected: this.data.currentTabbarIndex
 			})
 		}
+
+		const accountInfo = wx.getAccountInfoSync();
+		console.log(accountInfo)
+		// 保留自动显示版本号，仅正式版可用
+		// this.setData({
+		// 	version: accountInfo.miniProgram.version
+		// })
 	},
+
+	privacyPage() {
+		wx.openPrivacyContract({
+		  })
+	},
+
+	copyOfficialSite() {
+		wx.setClipboardData({
+			data: 'https://hipond.cc/',
+			success(res) {
+				wx.getClipboardData({
+					success(res) {
+						wx.showToast({
+							title: '网址已复制',
+							icon: 'success',
+							duration: 1500
+						})
+					}
+				})
+			}
+		})
+	},
+
+	copyOfficialEmail() {
+		wx.setClipboardData({
+			data: 'info@hipond.cc',
+			success(res) {
+				wx.getClipboardData({
+					success(res) {
+						wx.showToast({
+							title: '邮箱已复制',
+							icon: 'success',
+							duration: 1500
+						})
+					}
+				})
+			}
+		})
+	},
+
+	copyRedAccount() {
+		wx.setClipboardData({
+			data: '9615091209',
+			success(res) {
+				wx.getClipboardData({
+					success(res) {
+						wx.showToast({
+							title: '小红书已复制',
+							icon: 'success',
+							duration: 1500
+						})
+					}
+				})
+			}
+		})
+	}
 })
